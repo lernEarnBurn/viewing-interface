e34758f50472e4593864bbcb9a2e8ec355b2c014
lernEarnBurn <lerners124@gmail.com>
2023-11-29 21:34:20 +0200

    fixed route bug

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       <AnimatePresence mode="wait" initial={false}>
         <Routes location={location} key={location.pathname}>
           <Route
-            path="/"
+            path="/viewing-interface"
             element={
               loggedIn ? <Navigate to="/blogs" /> : <Navigate to="/log-in" />
             }
