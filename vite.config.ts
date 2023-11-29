4eb9f440e8f43d1314e5596038b5c3d717c1c1ce
lernEarnBurn <lerners124@gmail.com>
2023-11-29 15:19:51 +0200

    trying to deploy again

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: '/viewing-interface/',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/viewing-interface/'
 });
