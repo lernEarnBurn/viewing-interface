981d9146e6fde368645d4c52013ff684047b05d6
lernEarnBurn <lerners124@gmail.com>
2023-11-30 15:47:26 +0200

    log out also deletes other interface stuff

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,6 +15,8 @@ export function NavBar(props: navBarProps) {
     localStorage.removeItem("token");
     localStorage.removeItem("allBlogs");
     localStorage.removeItem("selectedBlog");
+    localStorage.removeItem("myBlogs");
+    localStorage.removeItem("selectedMyBlog");
     localStorage.removeItem("username");
     props.setLoggedIn(false);
     navigate("/viewing-interface/");
